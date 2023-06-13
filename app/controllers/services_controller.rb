e5e56036aa09025807cb815bd96f17d9b3bf724a
magdalena krasicka <krasickamagdalena@gmail.com>
2023-06-13 14:50:38 +0100

    generated routes

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,14 +1,40 @@
 class ServicesController < ApplicationController
+  # before_action :set_service, only: %i[edit, update, destroy]
 
-  def create
+  def index
+    # @services = Service.All
   end
 
-  def show
+  def new
+    # @service = Service.new
+  end
+
+  def create
+    # service = Service.new(service_params)
+    # service.save
+    # redirect_to service_path(service)
+  end
+
+  def edit
   end
 
   def update
+    # service.update(service_params)
+    # redirect_to service_path(service)
   end
 
   def destroy
+    # service.destroy
+    # redirect_to services_path, status: :see_other
+  end
+
+  private
+
+  def service_params
+    # params.require(:service).permit(:name, :taming)
+  end
+
+  def set_service
+    # @service = Service.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-
+  # get "services", to: "services#index"
+  # get "services/new", to: "services#new"
+  # post "services", to: "services#create"
+  # get "services/:id", to: "services#show", as: :service
+  # get "services/:id/edit", to: "services#edit"
+  # patch "services/:id", to: "services#update"
+  # delete "services/:id", to: "services#destroy"
+  resources :users do
+    resources :appointments do
+      resources :services
+    end
+  end
 end
